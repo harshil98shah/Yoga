dffdfae9a91678b24107b5903afeb4542d7d953c
harshil98shah <48246116+harshil98shah@users.noreply.github.com>
2019-09-07 19:48:02 +0530

    updated beginner yoga

--- app/src/main/java/com/example/myapplication/BeginnerYoga.java
+++ app/src/main/java/com/example/myapplication/BeginnerYoga.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class BeginnerYoga extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class BeginnerYoga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginner_yoga);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.color.colorBlue));
+
     }
 }
